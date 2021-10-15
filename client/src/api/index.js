1ee1c901b4f40056a8add534f317263c69b5e68c
Prabash <prabash.darshana@gmail.com>
2021-10-15 14:51:10 +0200

    Update index.js
    fixed issues with inital api calls

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 const inactive_time_mins = 60;
 
 export const login = async (data) =>  axios.post('login', data);
-export const getUser = async (id) => axios.get(`user/${id}`);
-export const getAllUsers = async () => axios.get(`users`);
-export const getInactiveUsers = async() => axios.get(`/get_inactive_users/${inactive_time_mins}`)
-export const updateUserStatus = async(data) => axios.post(`update_user_status`, data)
+export const getUser = async (id) => axios.get(`user/${id}`, { headers: { "x-access-token" : localStorage.getItem("access-token") }});
+export const getAllUsers = async () => axios.get(`users`, { headers: { "x-access-token" : localStorage.getItem("access-token") }});
+export const getInactiveUsers = async() => axios.get(`/get_inactive_users/${inactive_time_mins}`, { headers: { "x-access-token" : localStorage.getItem("access-token") }});
+export const updateUserStatus = async(data) => axios.post(`update_user_status`, data, { headers: { "x-access-token" : localStorage.getItem("access-token") }});
 
 const apis = {
     login,
